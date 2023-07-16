61da6997f6cdd86ff2a44e60b0ac77f24e186aec
Sunhwa Lee <3bearhug@gmail.com>
2023-07-16 15:41:17 -0400

    push

---
+++ Admin/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+using System;
+using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+
+namespace farmersMarket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string server = "localhost";
+        private string database = "farmersmarket";
+        private string uid = "root";
+        private string password = "root12345!";
+        private string connectionString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM products", connection);
+
+                try
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(cmd.ExecuteReader());
+
+                    ProductList.ItemsSource = dt.DefaultView;  // Bind the retrieved data to your 'ProductList' control
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Database Error: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("General Error: " + ex.Message);
+                }
+            }
+        }
+
+        //3. mySQ command generation and execution 
+        private void insert_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = Name.Text;
+            string id = idTextBox.Text;
+            string amountKg = amountTextBox.Text;
+            string pricePerKg = Name_Copy1.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO products (ProductName, ProductID, AmountKG, PricePerKG) VALUES (@productName, @id, @amountKg, @pricePerKg)";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@productName", productName);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@amountKg", amountKg);
+                    cmd.Parameters.AddWithValue("@pricePerKg", pricePerKg);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record inserted successfully");
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database error: " + ex.Message);
+                    }
+                }
+
+                LoadData();
+            }
+        }
+        private void select_Click_1(object sender, RoutedEventArgs e)
+        {
+            string id = idTextBox.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM products WHERE ProductID = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    try
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(cmd.ExecuteReader());
+
+                        ProductList.ItemsSource = dt.DefaultView;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void update_Click_1(object sender, RoutedEventArgs e)
+        {
+            string productName = Name.Text;
+            string id = idTextBox.Text;
+            string amountKg = amountTextBox.Text;
+            string pricePerKg = Name_Copy1.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE products SET ProductName = @productName, AmountKG = @amountKg, PricePerKG = @pricePerKg WHERE ProductID = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@productName", productName);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@amountKg", amountKg);
+                    cmd.Parameters.AddWithValue("@pricePerKg", pricePerKg);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record updated successfully");
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database error: " + ex.Message);
+                    }
+                }
+
+                LoadData();
+            }
+        }
+
+        private void delete_Click_1(object sender, RoutedEventArgs e)
+        {
+            string id = idTextBox.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM products WHERE ProductID = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record deleted successfully");
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database error: " + ex.Message);
+                    }
+                }
+
+                LoadData();
+            }
+        }
+
+        private void salesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sales sl = new Sales();
+
+            this.Hide();
+
+            sl.ShowDialog();
+
+            LoadData();
+            this.Show();
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Name.Text = "";
+            idTextBox.Text = "";
+            amountTextBox.Text = "";
+            Name_Copy1.Text = "";
+            LoadData();
+        }
+    }
+}
+
+
---
+++ Admin/Sales.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace farmersMarket 
+{
+    public partial class Sales : Window
+    {
+        public class Product
+        {
+            public string ProductName { get; set; }
+            public int ProductID { get; set; }
+            public double AmountKG { get; set; }
+            public decimal PricePerKG { get; set; }
+        }
+
+        public Sales()
+        {
+            InitializeComponent();
+            LoadProducts(); 
+        }
+
+        private void showme_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = Name.Text;
+            double requestedAmount = double.Parse(amountTextBox.Text);
+
+            Product product = GetProductByNameFromDB(productName);
+
+            if (product == null || product.AmountKG < requestedAmount)
+            {
+                MessageBox.Show("Invalid product or insufficient inventory.");
+                return;
+            }
+
+            decimal salesAmount = (decimal)requestedAmount * product.PricePerKG;
+
+            salesTextBox.Text = salesAmount.ToString();
+
+            product.AmountKG -= requestedAmount;
+
+            UpdateProductInDB(product);
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+
+            Name.Text = "";
+            amountTextBox.Text = "";
+            salesTextBox.Text = "";
+        }
+
+        private Product GetProductByNameFromDB(string productName)
+        {
+
+            string connectionString = "server=localhost;database=farmersMarket;user=root;password=root12345!";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Products WHERE ProductName = @ProductName";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductName", productName);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Product
+                            {
+                                ProductName = reader.GetString("ProductName"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                AmountKG = reader.GetDouble("AmountKG"),
+                                PricePerKG = reader.GetDecimal("PricePerKG")
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void UpdateProductInDB(Product product)
+        {
+            string connectionString = "server=localhost;database=farmersMarket;user=root;password=root12345!";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Products SET AmountKG = @AmountKG WHERE ProductName = @ProductName";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                    command.Parameters.AddWithValue("@AmountKG", product.AmountKG);
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("MySQL error: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("General error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void LoadProducts()
+        {
+            string connectionString = "server=localhost;database=farmersMarket;user=root;password=root12345!";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand("SELECT * FROM Products", connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<Product> products = new List<Product>();
+                        while (reader.Read())
+                        {
+                            products.Add(new Product()
+                            {
+                                ProductName = reader.GetString("ProductName"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                AmountKG = reader.GetDouble("AmountKG"),
+                                PricePerKG = reader.GetDecimal("PricePerKG")
+                            });
+                        }
+
+                        ProductList.ItemsSource = products;
+                    }
+                }
+            }
+        }
+    }
+}
